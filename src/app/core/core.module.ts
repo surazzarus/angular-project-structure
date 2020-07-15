d3300e78d939f1242dcd7852a937d5ed4e1f39ef
Suraj Shrestha <suraz.zarus@gmail.com>
2020-07-15 14:22:51 +0200

    Add guard and services (Auth, EventBus, Api) as part of core services
    - Other core services should be added here

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { HttpErrorInterceptor } from "./interceptors/http-error.interceptor";
+import { AuthenticationService } from "./services/authentication.service";
+import { EventBusService } from "./services/event-bus.service";
+import { AuthGuard } from "./guard/auth.guard";
+import { ApiService } from "./services/api.service";
 
 @NgModule({
   declarations: [],
@@ -9,6 +13,10 @@ import { HttpErrorInterceptor } from "./interceptors/http-error.interceptor";
     CommonModule
   ],
   providers: [
+    AuthGuard,
+    AuthenticationService,
+    EventBusService,
+    ApiService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpErrorInterceptor,
