dc06500a64c75d9cf41b554d4e8603fdcf7ed72b
Suraj Shrestha <suraz.zarus@gmail.com>
2020-07-15 16:44:19 +0200

    Add show and playlist services in their own respective modules

--- src/app/features/playlist/playlist.module.ts
+++ src/app/features/playlist/playlist.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { PlaylistRoutingModule } from './playlist-routing.module';
 import { ShowsTableComponent } from './components/shows-table/shows-table.component';
+import { PlaylistService } from "./services/playlist.service";
 
 
 @NgModule({
   declarations: [ShowsTableComponent],
+  providers: [PlaylistService],
   imports: [
     CommonModule,
     PlaylistRoutingModule
--- src/app/features/show/show.module.ts
+++ src/app/features/show/show.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ShowRoutingModule } from './show-routing.module';
 import { ClipsTableComponent } from './components/clips-table/clips-table.component';
+import { ShowService } from "./services/show.service";
 
 
 @NgModule({
   declarations: [ClipsTableComponent],
+  providers: [ShowService],
   imports: [
     CommonModule,
     ShowRoutingModule
